6c78a237983c98adbe8f1fbf8f139a4f223993de
sky7ech <sky_tech@live.fr>
2015-01-13 12:28:14 +0100

    Ecran principal (Momo)

--- src/main/java/appBureau/Main.java
+++ src/main/java/appBureau/Main.java
@@ -3,14 +3,21 @@ package appBureau;
 import javax.swing.JFrame;
 
 public class Main {
-
+	
+	
+	
+	
 	public static void main(String[] args) {
-
-		JFrame jf = new JFrame();
+		
+		JFrame jf= new JFrame();
 		jf.setSize(600, 800);
+		jf.setTitle("ShootMovie");
+		jf.setResizable(false);
 		jf.add(new PanelPrincipal());
 		jf.setVisible(true);
-
+		
+		
+		
 	}
 
 }
--- src/main/java/appBureau/PanelPrincipal.java
+++ src/main/java/appBureau/PanelPrincipal.java
@@ -1,14 +1,48 @@
 package appBureau;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class PanelPrincipal extends JPanel {
-
-	private JLabel logo;
-
+public class PanelPrincipal extends JPanel{
+	
+		private JLabel logo;
+		private PanelConnexion pc;
+	
+	
 	public PanelPrincipal() {
+		
+		
+		setLayout(new BorderLayout(100, 100));
+		
+		
+		logo= new JLabel(new ImageIcon("img/logo.png"));
+		add(logo,BorderLayout.NORTH);
+		
+		pc= new PanelConnexion();
+		add(new Connexion(),BorderLayout.CENTER);
+		
+		add(new JLabel(),BorderLayout.SOUTH);
+		add(new JLabel(),BorderLayout.EAST);
+		add(new JLabel(),BorderLayout.WEST);
+		
+	}
+	
+	
+	
 
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		Graphics2D g2d = (Graphics2D)g;
+
+
+	g2d.drawImage(new ImageIcon("img/background.jpg").getImage(), 0, 0, getWidth()	, getHeight(),null);
+	
 	}
 
 }
