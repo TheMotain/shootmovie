e339f9fffc4d1407b0f3ca3325d922d4238e8868
Mohamed Zarouri <mohamedz@hotmail.fr>
2015-01-13 10:15:37 +0100

    main

--- src/main/java/appBureau/Main.java
+++ src/main/java/appBureau/Main.java
@@ -1,5 +1,21 @@
 package appBureau;
 
+import javax.swing.JFrame;
+
 public class Main {
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+		JFrame jf= new JFrame();
+		jf.setSize(600, 800);
+		jf.add(new PanelPrincipal());
+		jf.setVisible(true);
+		
+		
+		
+	}
 
 }
