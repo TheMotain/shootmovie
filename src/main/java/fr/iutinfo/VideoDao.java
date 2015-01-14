06645ab7b93eafada401f2a8ba6cb943629c5607
Alex Dalencourt <alex.dalencourt@yahoo.fr>
2015-01-14 17:24:37 +0100

    BDD construite une majeur partie des methodes faites

--- src/main/java/fr/iutinfo/VideoDao.java
+++ src/main/java/fr/iutinfo/VideoDao.java
@@ -1,6 +1,9 @@
 package fr.iutinfo;
 
+import java.util.Iterator;
+
 import org.skife.jdbi.v2.sqlobject.Bind;
+import org.skife.jdbi.v2.sqlobject.SqlQuery;
 import org.skife.jdbi.v2.sqlobject.SqlUpdate;
 
 public interface VideoDao {
@@ -10,5 +13,15 @@ public interface VideoDao {
 	@SqlUpdate("INSERT INTO videos (titre,description,dateUpload,url,realisateur,note,compteur) VALUES (:titre,:description,:dateUpload,:url,:realisateur,'2.5',0)")
 	public void insertVideo(@Bind("titre") String titre, @Bind("description") String description, @Bind("dateUpload") String dateUpload, @Bind("url") String url, @Bind("realisateur") int realisateur);
 	
+	@SqlQuery("SELECT * FROM videos where id=:id")
+	public Video getVideo(@Bind("id") int id);
 	
+	@SqlQuery("SELECT * FROM videos")
+	public Iterator<Video> getAllVideo();
+	
+	@SqlUpdate("DROP TABLE videos")
+	public void dropTable();
+	
+	@SqlUpdate("DELETE FROM IF EXISTS videos where id=:id")
+	public void deleteVideo(@Bind("id") int id);
 }
