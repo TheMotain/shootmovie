0aee94533c85f3af7888cdde393d1bef79ea2db3
sky7ech <sky_tech@live.fr>
2015-01-15 15:48:42 +0100

    Requêtes profil

---
+++ src/main/java/fr/iutinfo/Profil.java
@@ -0,0 +1,44 @@
+package fr.iutinfo;
+
+public class Profil {
+	
+	
+	private int id;
+	private int id_utilisateur;
+	private String lien_background;
+	private String lien_avatar;
+	
+
+	public Profil(int id, int id_utilisateur, String lien_background, String lien_avatar) {
+		this.id=id;
+		this.id_utilisateur=id_utilisateur;
+		this.lien_background=lien_background;
+		this.lien_avatar=lien_avatar;
+		
+	}
+	
+	public Profil () {
+		
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public int getId_utilisateur() {
+		return id_utilisateur;
+	}
+	public void setId_utilisateur(int id_utilisateur) {
+		this.id_utilisateur = id_utilisateur;
+	}
+	
+	public String getLien_background() {
+		return lien_background;
+	}
+	public void setLien_avatar(String lien_avatar) {
+		this.lien_avatar = lien_avatar;
+	}
+}
---
+++ src/main/java/fr/iutinfo/ProfilDao.java
@@ -0,0 +1,36 @@
+package fr.iutinfo;
+
+import java.util.Iterator;
+
+import org.skife.jdbi.v2.sqlobject.Bind;
+import org.skife.jdbi.v2.sqlobject.SqlQuery;
+import org.skife.jdbi.v2.sqlobject.SqlUpdate;
+import org.skife.jdbi.v2.sqlobject.customizers.RegisterMapperFactory;
+import org.skife.jdbi.v2.tweak.BeanMapperFactory;
+
+public interface ProfilDao {
+	@SqlUpdate("CREATE TABLE IF NOT EXISTS profils (id INTEGER PRIMARY KEY, id_utilisateur INTEGER, lien_background TEXT, lien_avatar TEXT, FOREIGN KEY(id_utilisateur) REFERENCES users(id)))")
+	public void createTable();
+	
+	@SqlUpdate("INSERT INTO profils (id,id_utilisateur,lien_background,lien_avatar) VALUES (:id,:id_utilisateur,:lien_background,:lien_avatar)")
+	public void insertProfil(@Bind("id") int id, @Bind("id_utilisateur") int id_utilisateur, @Bind("lien_background") String lien_background, @Bind("lien_avatar") String lien_avatar);
+	
+	@SqlQuery("SELECT * FROM profils where id=:id")
+	@RegisterMapperFactory(BeanMapperFactory.class)
+	public Profil getProfil(@Bind("id") int id);
+	
+	@SqlQuery("SELECT * FROM commentaires where id_utilisateur=:id_utilisateur")
+	@RegisterMapperFactory(BeanMapperFactory.class)
+	public Profil getProfilByUser(@Bind("id_utilisateur") int id_utilisateur);
+	
+	@SqlQuery("SELECT * FROM profils")
+	@RegisterMapperFactory(BeanMapperFactory.class)
+	public Iterator<Profil> getAllProfil();
+	
+	@SqlUpdate("DROP TABLE profils")
+	public void dropTable();
+	
+	@SqlUpdate("DELETE FROM IF EXISTS profils where id=:id")
+	public void deleteVideo(@Bind("id") int id);
+}
+
